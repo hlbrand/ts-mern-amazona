fc84369fbcd8c2c7c6c1f7ca0a00a828e5640d3f
hlbrand <hlbrand2995@gmail.com>
2023-03-07 20:33:16 +0700

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
